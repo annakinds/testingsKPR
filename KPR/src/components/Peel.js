bf405765a6ee56caa1f443229903c88864f4cf1a
Ashley Vanneste <ashley.vanneste@student.howest.be>
2025-12-15 21:14:01 +0100

    fixed yellow markers

--- KPR/src/components/Peel.js
+++ KPR/src/components/Peel.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const peelContainer = document.querySelector('.peel-container');
   const peelEffect = peelContainer.querySelector('.peel-effect');
 
-  // Start with a very small triangle in the bottom right
   gsap.set(peelEffect, {
     clipPath: "polygon(100% 100%, 100% 100%, 100% 100%)"
   });
@@ -12,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const peelTransition = () => {
     gsap.to(peelEffect, {
       clipPath: "polygon(100% 100%, 0% 100%, 100% 0%)",
-      duration: 10,
+      duration: 1,
       ease: "power2.out",
     });
   };
