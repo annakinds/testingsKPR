323d348aca9f79a16f59c974661fdd2ef40815b5
Ashley Vanneste <ashley.vanneste@student.howest.be>
2025-12-16 13:27:24 +0100

    works

--- KPR/src/components/Peel.js
+++ KPR/src/components/Peel.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const cornerPeel = peelContainer.querySelector(".corner-peel");
 
 gsap.set(peelEffect, {
-  clipPath: "polygon(82.5% 82.5%, 100% 80%, 100% 100%, 82.5% 100%)"
+  clipPath: "polygon(80% 85%, 100% 85%, 100% 100%, 80% 100%)"
 });
 
 
@@ -15,22 +15,23 @@ gsap.set(peelEffect, {
   const peelTransition = () => {
     const tl = gsap.timeline();
 
+    tl.to(cornerPeel, {
+      width: "105%",
+      height: "105%",
+      duration: 1,
+      ease: "power2.out"
+    }, 0);
+
     tl.to(peelEffect, {
       clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       duration: 1,
       ease: "power2.out"
     }, 0);
 
-    tl.to(cornerPeel, {
-      width: "100%",
-      height: "100%",
-      duration: 1,
-      ease: "power2.out"
-    }, 0);
-
     tl.to(triangleFill, {
-      x: "-100%",
-      y: "-250%",
+      x: "-120%",
+      y: "-260%",
+      scale: 1.2,
       duration: 1,
       ease: "power2.in"
     }, 0.6);
